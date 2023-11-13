fabac3ba5627ff46b775d44c06c254adbfc925e5
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-13 13:17:55 -0500

    Create the Foods data model by migrating the data base

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_175207) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_13_181636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price", precision: 10, scale: 2, null: false
+    t.integer "quantity", null: false
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_foods_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_175207) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users", column: "users_id"
 end
