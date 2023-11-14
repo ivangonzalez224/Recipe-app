b6b6c29aa63a284714ac8496715fc2a6d6d96cca
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-14 17:34:12 -0500

    Create the new method to display a form to create a new food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,24 @@
 class FoodsController < ApplicationController
+  before_action :find_food, only: %i[show edit update destroy]
   def index
     @foods = if params[:order_by] == 'name'
                current_user.foods.order('LOWER(name)')
              else
                current_user.foods
              end
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  private
+
+  def find_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
   end
 end
