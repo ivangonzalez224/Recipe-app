cd73bda35d52920c060c506220024452c7b58a68
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-14 14:07:56 -0500

    Create a method to return all foods that are used in all of the user's recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,17 @@ class User < ApplicationRecord
 
   has_many :recipes
   has_many :foods
-  
+
   validates :name, presence: true
+
+  # Returning a list of all foods that are used in all of the user's recipes
+  def all_food_used
+    foods = []
+    recipes.each do |recipe|
+      recipe.foods.each do |food|
+        foods << food
+      end
+    end
+    foods
+  end
 end
