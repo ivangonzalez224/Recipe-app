57e57f7c17b14eafa09967dfbd11cb6b6fae900c
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-14 17:21:19 -0500

    Create the index action in the food controller to list all foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,9 @@
 class FoodsController < ApplicationController
-  def index; end
-
-  def show; end
+  def index
+    @foods = if params[:order_by] == 'name'
+               current_user.foods.order('LOWER(name)')
+             else
+               current_user.foods
+             end
+  end
 end
