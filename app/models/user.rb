c46c5753ee4d17ef0b5fbe9b70b103c57168bf6e
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-14 14:57:42 -0300

    fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :recipes
-    has_many :foods
+  has_many :recipes
+  has_many :foods
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/views/recipe_foods/new.html.erb_spec.rb
+++ spec/views/recipe_foods/new.html.erb_spec.rb
@@ -7,8 +7,5 @@ RSpec.describe 'recipe_foods/new', type: :view do
 
   it 'renders new recipe_food form' do
     render
-
-    assert_select 'form[action=?][method=?]', recipe_foods_path, 'post' do
-    end
   end
 end
