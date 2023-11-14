22ee5744196beabfec781f3e1b199b6c8722c6e2
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-14 14:52:17 -0500

    Create a method to get the price of a given food name

--- app/models/food.rb
+++ app/models/food.rb
@@ -7,4 +7,10 @@ class Food < ApplicationRecord
   validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :quantity, presence: true, numericality: { greater_than: 0 }
+
+  # Returning the price of a given food name
+  def self.getting_price(food_name)
+    food = Food.find_by(name: food_name)
+    food ? food.price : 0
+  end
 end
