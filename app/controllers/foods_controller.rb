5ab1bad138e07b608bd270d2e2591f8167a898b5
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-14 20:32:15 -0500

    Create a method to update a food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -25,4 +25,18 @@ class FoodsController < ApplicationController
   def clean_name(food_name)
     food_name.strip.downcase.titleize.singularize
   end
+
+  def update_food(old_food)
+    update_food_attributes(old_food)
+    redirect_to foods_path, notice: 'The food was successfully updated.'
+  end
+
+  def update_food_attributes(food)
+    new_quantity = food.quantity + food_params[:quantity].to_i
+    old_cost = food.price * food.quantity
+    new_cost = food_params[:price].to_f * food_params[:quantity].to_i
+    total_cost = old_cost + new_cost
+    new_unit_price = total_cost / new_quantity
+    food.update(quantity: new_quantity, price: new_unit_price)
+  end
 end
