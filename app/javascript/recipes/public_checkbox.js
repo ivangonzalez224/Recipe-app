aa925746805f5342c2aec01cfc4ea9b18b2f8e61
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-17 14:12:01 -0300

    add style to toggle button

--- app/javascript/recipes/public_checkbox.js
+++ app/javascript/recipes/public_checkbox.js
@@ -1,10 +1,11 @@
+console.log('Hello from public_checkbox.js');
 document.addEventListener('turbo:load', function () {
   const checkbox = document.querySelector('.public-checkbox');
-
   if (checkbox) {
     checkbox.addEventListener('change', function () {
       const recipeId = checkbox.dataset.recipeId;
       const newValue = checkbox.checked;
+      console.log("change");
 
       // Send an AJAX request to update the recipe.public value
       fetch(`/recipes/${recipeId}`, {
