980fb31e4a4b3fdeaaf889159a9ec270f89a4d23
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-17 13:30:33 -0500

    Test when the name is not present in a food

--- spec/models/foods_spec.rb
+++ spec/models/foods_spec.rb
@@ -13,5 +13,10 @@ RSpec.describe Food, type: :model do
         food = Food.new(name: 'Apple', measurement_unit: 'g', price: 3, quantity: 500)
         expect(food).to_not be_valid
       end
+
+      it 'is not valid without a name' do
+        food = Food.new(user: user, measurement_unit: 'g', price: 5, quantity: 1000)
+        expect(food).to_not be_valid
+      end  
   end  
 end    
