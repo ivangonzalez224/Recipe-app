79e95a4d7beab225cec1341b2e7e8cd9ed690c48
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-17 13:45:45 -0500

    Test if the foods path render the correct content without log in

---
+++ spec/features/foods_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'foods path', type: :feature do
+  it 'displays the correct content when visits foods without sign in' do
+    visit foods_path
+
+    expect(page).to have_content('You need to sign in or sign up before continuing')
+  end
+end  
