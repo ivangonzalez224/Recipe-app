8d2e277fc26630761a2595a4b4124d01fb9ac429
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-17 13:22:57 -0500

    Test to validate if the name attribute is present when creating a user

---
+++ spec/models/users_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'is valid with valid attributes' do
+    user = User.new(
+      name: 'Kobe',
+      email: 'kobe@example.com',
+      password: 'kobe123',
+      password_confirmation: 'kobe123'
+    )
+    expect(user).to be_valid
+  end
+end  
