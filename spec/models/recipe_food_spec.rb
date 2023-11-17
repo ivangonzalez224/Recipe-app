56666ca866ddc43c64aa1af0a2d65f09a9c513d1
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-17 18:52:20 -0300

    fix linters

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe RecipeFood, type: :model do
   describe 'validations' do
     let(:user) { User.new(name: 'Kobe', id: 1) }
-    let(:recipe) { Recipe.new(id: 2, user: user, name: 'Apple Pie', preparation_time: 30, cooking_time: 60, description: 'A delicious apple pie', public: true) }
+    let(:recipe) do
+      Recipe.new(id: 2, user: user, name: 'Apple Pie', preparation_time: 30, cooking_time: 60,
+                 description: 'A delicious apple pie', public: true)
+    end
     let(:food) { Food.new(id: 3, user: user, name: 'Apple', measurement_unit: 'g', price: 3, quantity: 500) }
 
     it 'is valid with valid attributes' do
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -5,24 +5,27 @@ RSpec.describe Recipe, type: :model do
     let(:user) { User.new(name: 'Kobe') }
 
     it 'is valid with valid attributes' do
-      recipe = Recipe.new(user: user, name: 'Apple Pie', preparation_time: 30, cooking_time: 60, description: 'A delicious apple pie', public: true)
+      recipe = Recipe.new(user: user, name: 'Apple Pie', preparation_time: 30, cooking_time: 60,
+                          description: 'A delicious apple pie', public: true)
       expect(recipe).to be_valid
     end
 
     it 'is not valid without a name' do
-      recipe = Recipe.new(user: user, preparation_time: 30, cooking_time: 60, description: 'A delicious apple pie', public: true)
+      recipe = Recipe.new(user: user, preparation_time: 30, cooking_time: 60, description: 'A delicious apple pie',
+                          public: true)
       expect(recipe).to_not be_valid
     end
 
     it 'is not valid with a negative preparation time' do
-      recipe = Recipe.new(user: user, name: 'Apple Pie', preparation_time: -30, cooking_time: 60, description: 'A delicious apple pie', public: true)
+      recipe = Recipe.new(user: user, name: 'Apple Pie', preparation_time: -30, cooking_time: 60,
+                          description: 'A delicious apple pie', public: true)
       expect(recipe).to_not be_valid
     end
 
     it 'is not valid with a negative cooking time' do
-      recipe = Recipe.new(user: user, name: 'Apple Pie', preparation_time: 30, cooking_time: -60, description: 'A delicious apple pie', public: true)
+      recipe = Recipe.new(user: user, name: 'Apple Pie', preparation_time: 30, cooking_time: -60,
+                          description: 'A delicious apple pie', public: true)
       expect(recipe).to_not be_valid
     end
   end
-
 end
