2cbe54a4271d3c475998a7b39aa98c5125efaf46
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-17 13:31:15 -0500

    Test when the food price is negative

--- spec/models/foods_spec.rb
+++ spec/models/foods_spec.rb
@@ -17,6 +17,11 @@ RSpec.describe Food, type: :model do
       it 'is not valid without a name' do
         food = Food.new(user: user, measurement_unit: 'g', price: 5, quantity: 1000)
         expect(food).to_not be_valid
-      end  
+      end
+      
+      it 'is not valid with a negative price' do
+        food = Food.new(user: user, name: 'Apple', measurement_unit: 'g', price: -1.25, quantity: 500)
+        expect(food).to_not be_valid
+      end
   end  
 end    
